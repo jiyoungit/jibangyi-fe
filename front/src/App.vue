368c8796577dc86d8f91b6af577c71ecdba00d13
박지영 <20181621@kookmin.ac.kr>
2023-11-14 17:59:36 +0900

    feat: 지도  api 호출, 로고 이미지

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import Header from './common/Header.vue';
-import Footer from './common/Footer.vue';
+import Header from './components/common/Header.vue';
+import Footer from './components/common/Footer.vue';
 import MainView from './views/MainView.vue';
 </script>
 
@@ -18,6 +18,7 @@ import MainView from './views/MainView.vue';
 html {
   scroll-behavior: smooth;
 }
+
 .container {
   display: flex;
   flex-direction: column;
@@ -26,24 +27,27 @@ html {
   min-width: 1200px;
   position: relative;
 }
+
 @font-face {
   font-family: 'Noto Sans KR';
   src: url('assets/fonts/NotoSansKR-Regular.otf');
   font-weight: 400;
 }
+
 @font-face {
   font-family: 'Noto Sans KR';
   src: url('assets/fonts/NotoSansKR-Medium.otf');
   font-weight: 500;
 }
+
 @font-face {
   font-family: 'Noto Sans KR';
   src: url('assets/fonts/NotoSansKR-Light.otf');
   font-weight: 300;
 }
+
 @font-face {
   font-family: 'Noto Sans KR';
   src: url('assets/fonts/NotoSansKR-Bold.otf');
   font-weight: 600;
-}
-</style>
+}</style>
--- front/src/views/DealView.vue
+++ front/src/views/DealView.vue
@@ -1,4 +1,9 @@
-<script></script>
-<template></template>
+<script setup>
+import AptMap from '../components/apt/AptMap.vue';
+import AptDetail from '../components/apt/AptDetail.vue';
+</script>
+<template>
+  <AptMap />
+</template>
 
 <style scoped></style>
