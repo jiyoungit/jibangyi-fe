9774d9e875006866aba33177167b679af34f3169
jjmsrc <jjmsrc98@gmail.com>
2023-11-15 01:09:46 +0900

    feat: front - houseinfo marker by center coord

--- front/src/api/apt.js
+++ front/src/api/apt.js
@@ -1,14 +1,17 @@
-import { localAxios } from "@/util/http-commons";
+import { localAxios } from '@/util/http-commons'
 
-const local = localAxios();
-const PREFIX = '/house';
+const local = localAxios()
+const PREFIX = '/house'
 
 function findDongCode(dongName, success, fail) {
-  local.get(`${PREFIX}/dongcodes/${dongName}`).then(success).catch(fail);
+  local.get(`${PREFIX}/dongcodes/${dongName}`).then(success).catch(fail)
 }
 
 function listAptInfos(dongCode, success, fail) {
-  local.get(`${PREFIX}/apt-infos`, { params: {dongCode,} }).then(success).catch(fail);
+  local
+    .get(`${PREFIX}/apt-infos`, { params: { dongCode } })
+    .then(success)
+    .catch(fail)
 }
 
 function listAptDealsByAptCode(param, success, fail) {
@@ -19,11 +22,26 @@ function listAptDealsByAptCode(param, success, fail) {
   //   "limit": 0,
   //   "offset": 0
   // }
-  local.get(`${PREFIX}/apt-deals`, {params: param}).then(success).catch(fail);
+  local.get(`${PREFIX}/apt-deals`, { params: param }).then(success).catch(fail)
+}
+
+function listAptInfosByCoold(param, success, fail) {
+  // const param = {
+  //   "lat": "",
+  //   "lng": "",
+  //   "range": 0,
+  //   "limit": 0,
+  //   "offset": 0
+  // }
+  local
+    .get(`${PREFIX}/apt-infos/coold`, { params: param })
+    .then(success)
+    .catch(fail)
 }
 
 export {
   findDongCode,
   listAptInfos,
   listAptDealsByAptCode,
+  listAptInfosByCoold,
 }
