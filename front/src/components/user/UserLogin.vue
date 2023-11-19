11230db980c30ad54f2d23ae58513a9cebe398cb
parkyolo <20181621@kookmin.ac.kr>
2023-11-19 22:58:00 +0900

    feat: 로그인, 회원가입 vue

--- front/src/components/user/UserLogin.vue
+++ front/src/components/user/UserLogin.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue';
 
-let message = ref("");
-let userid = ref("");
-let userpw = ref("");
+let message = ref('');
+let userid = ref('');
+let userpw = ref('');
 
 ////////////////////// 예외 처리
 // document.querySelector("#btn-mv-join").addEventListener("click", function () {
@@ -24,14 +24,51 @@ let userpw = ref("");
 // });
 
 function userLogin() {
-  console.log("사용자 정보: ", userid, userpw);
+  console.log('사용자 정보: ', userid, userpw);
 }
 </script>
 
 <template>
   <section>
-
+    <div>
+      <header><h1>로그인</h1></header>
+      <form action="">
+        <div>
+          <header>
+            <h1>아이디</h1>
+          </header>
+          <div class="input">
+            <div>
+              <div>
+                <input type="text" name="id" id="id" placeholder="아이디 입력" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <header>
+            <h1>비밀번호</h1>
+          </header>
+          <div class="input">
+            <div>
+              <div>
+                <input type="text" name="id" id="id" placeholder="비밀번호 입력" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <button type="submit">
+          <span>로그인</span>
+        </button>
+      </form>
+      <div class="btnWrap">
+        <button>비밀번호 변경</button>
+        <button>신규 회원가입</button>
+      </div>
+    </div>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+@import '@/assets/scss/user/login.scss';
+</style>
