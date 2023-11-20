0b2fc94f8952071f99418c96526ecf6333234530
박지영 <20181621@kookmin.ac.kr>
2023-11-20 17:28:20 +0900

    feat: 폰트 변경

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
-import Header from './components/common/Header.vue';
-import Footer from './components/common/Footer.vue';
-import MainView from './views/MainView.vue';
+import Header from '@/components/common/Header.vue';
+import Footer from '@/components/common/Footer.vue';
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+const location = useRoute();
+
+let isDealPage = ref(false);
+watch(location, () => {
+  if (location.path === '/deal') isDealPage.value = true;
+  else isDealPage.value = false;
+})
 </script>
 
 <template>
@@ -9,7 +17,10 @@ import MainView from './views/MainView.vue';
     <div class="container">
       <Header />
       <router-view />
-      <Footer />
+      <div v-if="location.path === '/deal'"></div>
+      <div v-else>
+        <Footer />
+      </div>
     </div>
   </div>
 </template>
@@ -29,25 +40,56 @@ html {
 }
 
 @font-face {
-  font-family: 'Noto Sans KR';
-  src: url('assets/fonts/NotoSansKR-Regular.otf');
-  font-weight: 400;
+  font-family: 'Apple SD Gothic';
+  src: url('assets/fonts/AppleSDGothicNeoT.ttf');
+  font-weight: 100;
 }
 
 @font-face {
-  font-family: 'Noto Sans KR';
-  src: url('assets/fonts/NotoSansKR-Medium.otf');
-  font-weight: 500;
+  font-family: 'Apple SD Gothic';
+  src: url('assets/fonts/AppleSDGothicNeoUL.ttf');
+  font-weight: 200;
 }
 
 @font-face {
-  font-family: 'Noto Sans KR';
-  src: url('assets/fonts/NotoSansKR-Light.otf');
+  font-family: 'Apple SD Gothic';
+  src: url('assets/fonts/AppleSDGothicNeoL.ttf');
   font-weight: 300;
 }
 
 @font-face {
-  font-family: 'Noto Sans KR';
-  src: url('assets/fonts/NotoSansKR-Bold.otf');
+  font-family: 'Apple SD Gothic';
+  src: url('assets/fonts/AppleSDGothicNeoR.ttf');
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: 'Apple SD Gothic';
+  src: url('assets/fonts/AppleSDGothicNeoM.ttf');
+  font-weight: 500;
+}
+
+@font-face {
+  font-family: 'Apple SD Gothic';
+  src: url('assets/fonts/AppleSDGothicNeoSB.ttf');
   font-weight: 600;
-}</style>
+}
+
+@font-face {
+  font-family: 'Apple SD Gothic';
+  src: url('assets/fonts/AppleSDGothicNeoB.ttf');
+  font-weight: 700;
+}
+
+@font-face {
+  font-family: 'Apple SD Gothic';
+  src: url('assets/fonts/AppleSDGothicNeoEB.ttf');
+  font-weight: 800;
+}
+
+@font-face {
+  font-family: 'Apple SD Gothic';
+  src: url('assets/fonts/AppleSDGothicNeoH.ttf');
+  font-weight: 900;
+}
+</style>
