765ae04e8c25f5cf06d84eb4c5c922ba6ac68b96
박지영 <20181621@kookmin.ac.kr>
2023-11-22 12:51:26 +0900

    feat: 게시판 등록, 수정, 조회

--- front/src/components/board/BoardList.vue
+++ front/src/components/board/BoardList.vue
@@ -27,62 +27,24 @@ onMounted(() => {
       console.log(error);
     })
 })
-
-
-
-//////////////// 조회 버튼
-// let titles = document.querySelectorAll(".article-title");
-// titles.forEach(function (title) {
-//   title.addEventListener("click", function () {
-//     console.log(this.getAttribute("data-no"));
-//     location.href =
-//       "${root}/article?action=view&articleno=" + this.getAttribute("data-no");
-//   });
-// });
-
-//////////////// 새 글 등록 버튼
-// document
-//   .querySelector("#btn-mv-register")
-//   .addEventListener("click", function () {
-//     location.href = "${root}/article?action=mvwrite";
-//   });
-
-//////////////// 검색 버튼
-// document.querySelector("#btn-search").addEventListener("click", function () {
-//   let form = document.querySelector("#form-search");
-//   form.setAttribute("action", "${root}/article");
-//   form.submit();submit
-// });
-
-//////////////// 뭔지 모름
-// let pages = document.querySelectorAll(".page-link");
-// pages.forEach(function (page) {
-//   page.addEventListener("click", function () {
-//     console.log(this.parentNode.getAttribute("data-pg"));
-//     document.querySelector("#p-action").value = "list";
-//     document.querySelector("#p-pgno").value =
-//       this.parentNode.getAttribute("data-pg");
-//     document.querySelector("#p-key").value = "${param.key}";
-//     document.querySelector("#p-word").value = "${param.word}";
-//     document.querySelector("#form-param").submit();
-//   });
-// });
-
 </script>
 
 <template>
   <section>
     <div>
+      <div class="registBtn">
+        <RouterLink :to="{ name: 'regist' }"><v-btn rounded="lg">문의하기</v-btn></RouterLink>
+      </div>
       <table>
         <thead></thead>
         <tbody>
           <!-- "qnaNo": 25,
-        "userId": "ssafy",
-        "userName": "김싸피",
-        "subject": "string1",
-        "content": null,
-        "hit": 0,
-        "registerTime": "2023-11-21 12:30:19" -->
+                          "userId": "ssafy",
+                          "userName": "김싸피",
+                          "subject": "string1",
+                          "content": null,
+                          "hit": 0,
+                          "registerTime": "2023-11-21 12:30:19" -->
           <tr v-for="article in articles" :key="article.qnaNo">
             <td class="articleSubject">
               <RouterLink :to="{ name: 'detail', params: { 'id': article.qnaNo } }">
