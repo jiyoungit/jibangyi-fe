c47003edb6e496abd7c67a1bcabde600e63d273f
박지영 <20181621@kookmin.ac.kr>
2023-11-22 17:55:04 +0900

    feat: 매물 비교 리스트 띄우기

---
+++ front/src/views/DealCompareView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MyHouseList from '@/components/myhouse/MyHouseList.vue';
+import MyHouseCompare from '@/components/myhouse/MyHouseCompare.vue';
+</script>
+
+<template>
+  <div>
+    <MyHouseList />
+    <MyHouseCompare />
+  </div>
+</template>
+
+<style scoped></style>
